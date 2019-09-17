7ab2885bda4f0589001854836e861d4545b48cbf
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2019-09-17 12:57:00 -0400

    Update some go lint issues
    CamelCase naming conventions, comment conventions,
    change function to be unexported.

--- status.go
+++ status.go
@@ -6,25 +6,26 @@ import (
 	"time"
 )
 
-const ERROR_STATUS = "ERROR"
+// ErrorStatus represents the default Site status when http errors occur
+const ErrorStatus = "ERROR"
 
 // Hit the site URL with a HEAD request and update appropriate fields
 func checkSiteStatus(s Site) {
 	// If this is an invalid Site config, don't even bother checking
-	if ERROR_STATUS == s.Status {
+	if ErrorStatus == s.Status {
 		return
 	}
 
 	resp, err := http.Head(s.URL)
 	if err != nil {
-		s.Status = ERROR_STATUS
+		s.Status = ErrorStatus
 		log.Println("[ERROR] Problem creating new HTTP request:", err.Error())
 		return
 	}
 
 	s.Status = resp.Status
 
-	err = s.LogStat(resp.StatusCode)
+	err = s.logStat(resp.StatusCode)
 	if err != nil {
 		log.Println("[WARN] Error logging stat:", err.Error())
 	}
@@ -32,7 +33,7 @@ func checkSiteStatus(s Site) {
 	log.Println("[DEBUG]", s.URL, s.Status)
 }
 
-func (s Site) LogStat(status int) error {
+func (s Site) logStat(status int) error {
 	var check Check
 	check.SiteId = s.Id
 	check.Response = status
