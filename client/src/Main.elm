db24d27f10bec9dee36dcabd31e7ce3f66017e51
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2019-09-18 11:48:43 -0400

    Show responsive customized line chart

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -8,6 +8,19 @@ import Http
 import Json.Decode exposing (Decoder, float, int, list, string, succeed)
 import Json.Decode.Pipeline exposing (hardcoded, optional, required)
 import LineChart
+import LineChart.Area as LCArea
+import LineChart.Axis as LCAxis
+import LineChart.Axis.Intersection as LCIntersection
+import LineChart.Colors as LCColors
+import LineChart.Container as LCContainer
+import LineChart.Dots as LCDots
+import LineChart.Events as LCEvents
+import LineChart.Grid as LCGrid
+import LineChart.Interpolation as LCInterpolation
+import LineChart.Junk as LCJunk
+import LineChart.Legends as LCLegends
+import LineChart.Line as LCLine
+import Time
 
 
 apiUrlPrefix : String
@@ -105,6 +118,7 @@ viewSiteDetails maybeSite =
         Just site ->
             div [ class "site-details" ]
                 [ h2 [] [ text site.url ]
+                , viewSiteChecksChart site.checks
                 , LineChart.view1 (\c -> toFloat c.createdAt) (\c -> toFloat c.responseTime) site.checks
                 , table []
                     [ thead []
@@ -120,6 +134,28 @@ viewSiteDetails maybeSite =
 
         Nothing ->
             div [ class "site-details empty" ] []
+
+
+viewSiteChecksChart : List SiteCheck -> Html Msg
+viewSiteChecksChart checks =
+    LineChart.viewCustom chartConfig [ LineChart.line LCColors.blue LCDots.circle "SITE" checks ]
+
+
+chartConfig : LineChart.Config SiteCheck Msg
+chartConfig =
+    { y = LCAxis.full 450 "Response Time in ms" (\c -> toFloat c.responseTime)
+    , x = LCAxis.time Time.utc 1270 "Time" (\c -> toFloat c.createdAt)
+    , container = LCContainer.default "uptime"
+    , interpolation = LCInterpolation.monotone
+    , intersection = LCIntersection.default
+    , legends = LCLegends.default
+    , events = LCEvents.default
+    , junk = LCJunk.default
+    , grid = LCGrid.dots 1 LCColors.gray
+    , area = LCArea.normal 1.0
+    , line = LCLine.default
+    , dots = LCDots.custom (LCDots.empty 5 1)
+    }
 
 
 siteCheckTableRow : SiteCheck -> Html Msg
