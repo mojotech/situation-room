385564e84b4cdc1c653b176b62a9896d0744bcfe
Zach Robert <zrobert@mojotech.com>
2022-04-04 11:29:38 -0400

    [chore] init elixir boilerplate for rest_server and ecto/postgrex deps

---
+++ situation_room/apps/rest_server/lib/rest_server.ex
@@ -0,0 +1,18 @@
+defmodule RESTserver do
+  @moduledoc """
+  Documentation for `RESTserver`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RESTserver.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ situation_room/apps/rest_server/lib/rest_server/application.ex
@@ -0,0 +1,20 @@
+defmodule RESTserver.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: RESTserver.Worker.start_link(arg)
+      # {RESTserver.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: RESTserver.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
