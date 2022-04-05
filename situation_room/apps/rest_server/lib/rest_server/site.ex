934ae0794a6d42306806e8bf3217848f2c3823d5
Zach Robert <zrobert@mojotech.com>
2022-04-05 14:50:26 -0400

    [feat] add parameter validation to site schema

--- situation_room/apps/rest_server/lib/rest_server/site.ex
+++ situation_room/apps/rest_server/lib/rest_server/site.ex
@@ -1,14 +1,23 @@
 #import RESTserver.PingHistory
-#import RESTserver.Active
 defmodule RESTserver.Site do
   use Ecto.Schema
+  import Ecto.Changeset
+  #import RESTserver.PingHistory
+  #import RESTserver.Active
 
   schema "site" do
-    #field :id, :integer
+    # Ignore the ID Field -> it is default "id" and serial integer
     field :endpoint, :string
     field :name, :string
     field :created_at, :naive_datetime_usec
     #has_many :ping_history, PingHistory
     #has_one :active, Active
   end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:endpoint, :name])
+    |> validate_required([:endpoint, :name])
+  end
+
 end
