d0cbd33776f29d4df738dfa5c27a6f03e082d0ba
Zach Robert <zrobert@mojotech.com>
2022-04-05 14:03:27 -0400

    [fix] site compilation error

--- situation_room/apps/rest_server/lib/rest_server/site.ex
+++ situation_room/apps/rest_server/lib/rest_server/site.ex
@@ -1,3 +1,5 @@
+#import RESTserver.PingHistory
+#import RESTserver.Active
 defmodule RESTserver.Site do
   use Ecto.Schema
 
@@ -6,7 +8,7 @@ defmodule RESTserver.Site do
     field :endpoint, :string
     field :name, :string
     field :created_at, :naive_datetime_usec
-    has_many :ping_history, PingHistory
-    has_one :active, Active
+    #has_many :ping_history, PingHistory
+    #has_one :active, Active
   end
 end
