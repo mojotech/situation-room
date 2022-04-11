994f183b8f0bd1c7fee2dcba12af94b093e48197
Zach Robert <zrobert@mojotech.com>
2022-04-11 11:56:26 -0400

    [chore] add elixir structure generated from

---
+++ lib/situation_room.ex
@@ -0,0 +1,18 @@
+defmodule SituationRoom do
+  @moduledoc """
+  Documentation for `SituationRoom`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SituationRoom.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/situation_room_test.exs
@@ -0,0 +1,8 @@
+defmodule SituationRoomTest do
+  use ExUnit.Case
+  doctest SituationRoom
+
+  test "greets the world" do
+    assert SituationRoom.hello() == :world
+  end
+end
