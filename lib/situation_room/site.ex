0482d02f2d5c1f19539854e7e53aa53ee6f37e1b
Zach Robert <zrobert@mojotech.com>
2022-04-13 08:48:05 -0400

    [feat] add basic site changeset

--- lib/situation_room/site.ex
+++ lib/situation_room/site.ex
@@ -1,8 +1,16 @@
 defmodule SituationRoom.Site do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "sites" do
     field :endpoint, :string
     field :name, :string
   end
+
+  def changeset(site, params) do
+    site
+    |> cast(params, [:endpoint, :name])
+    |> validate_required([:endpoint, :name])
+  end
+
 end
