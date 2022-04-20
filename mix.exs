c636c98fdb1ae56ae3849f47135e12b0a532692c
Zach Robert <zrobert@mojotech.com>
2022-04-20 14:23:36 -0400

    [refactor] add version control and update mix

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SituationRoom.MixProject do
     [
       app: :situation_room,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.13.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
@@ -15,16 +15,17 @@ defmodule SituationRoom.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {SituationRoom.Application, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug, "~> 1.13.5"},
       {:ecto, "~> 3.7.2"},
       {:ecto_sql, "~> 3.7.2"},
+      {:plug_cowboy, "~> 2.5.2"},
       {:postgrex, "~> 0.16.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
@@ -33,6 +34,11 @@ defmodule SituationRoom.MixProject do
 
   defp aliases do
     # Alises that auto setup the database when conducting tests
-    [test: ["ecto.create --quiet", "ecto.migrate", "test"]]
+    [
+      test: [
+        "ecto.create --quiet",
+        "ecto.migrate", "test"
+      ]
+    ]
   end
 end
