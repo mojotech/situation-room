62c201a23cec215a6cfb095d18ba9ab0cfe26d04
Zach Robert <zrobert@mojotech.com>
2022-04-20 15:15:13 -0400

    [test] add test coverage for query and delete

--- test/api/v1/validator_test.exs
+++ test/api/v1/validator_test.exs
@@ -2,9 +2,13 @@ defmodule SituationRoom.ValidatorTest do
   use ExUnit.Case, async: true
   import SituationRoom.Site
 
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(SituationRoom.Repo)
+  end
+
   @test_suites [
-    {"MojoTech Main", "https://mojotech.com", true},
-    {"MojoTech API", "http://api.mojotech.com", true},
+    {"MojoTech Main", "https://youtube.com", true},
+    {"MojoTech API", "http://mail.google.com", true},
     {"x", "https://x.com", true},
     {"Flop", "pets.com", false},
     {"", "https://google.com", false},
@@ -27,5 +31,22 @@ defmodule SituationRoom.ValidatorTest do
 
       assert result.valid? == elem(test_case, 2)
     end
+
+    test "Test site creation, query, and deletion #{elem(i, 1)}" do
+      test_case = unquote(mac)
+      expected_result = elem(test_case, 2)
+      create_res = create_site(elem(test_case, 0), elem(test_case, 1))
+
+      if expected_result do
+        assert {:ok, _} = create_res
+        query_res = get_site(name: elem(test_case, 0))
+        # Add assert statement to test query
+        # assert query_res[:endpoint] = elem(test_case, 1)
+        assert {:ok, _} = delete_site(endpoint: elem(test_case, 1))
+      else
+        assert {:error, _} = create_res
+      end
+    end
+
   end
 end
