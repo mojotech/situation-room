26cbf28172a00f0d988405d1d153001608f3c0b0
Zach Robert <zrobert@mojotech.com>
2022-05-11 10:16:11 -0400

    [feat] router and controller organization - add get site routes

--- lib/router.ex
+++ lib/router.ex
@@ -1,6 +1,5 @@
 defmodule SituationRoom.Router do
   use Plug.Router
-  alias SituationRoom.Site
 
   if Mix.env() == :dev do
     use Plug.Debugger
@@ -14,55 +13,12 @@ defmodule SituationRoom.Router do
   plug(:dispatch)
 
   get "/ping" do
-    send_resp(conn, 200, "Mojo Pong")
+    send_resp(conn, 200, "{:ok, Mojo Pong}")
   end
 
-  # get "site" do
-  #   Site.get_all_sites()
-  #   |> clean_query()
-  #   |> case do
-  #     {:ok, content} ->
-  #       send_resp(conn, 200, "[]")
-  #     _ ->
-  #       send_resp(conn, 500, "Unable to get sites")
-  #   end
-  # end
-
-  # This route can be hit when forward slashes are encoded as %2F
-  post "/site/:name/:endpoint" do
-    case res = Site.create_site(conn.params["name"], conn.params["endpoint"]) do
-      {:ok, _} ->
-        send_resp(conn, 201, "{:ok, 'Successfully created #{conn.params["endpoint"]}'}")
-
-      # We need to pattern match errors for invalid URL
-      {:error,
-       %Ecto.Changeset{
-         action: nil,
-         changes: %{},
-         errors: [endpoint: errors],
-         data: _,
-         valid?: false
-       }} ->
-        send_resp(conn, 400, "{:error, '#{elem(errors, 0)}'}")
-
-      _ ->
-        send_resp(conn, 400, "{:error, 'Unable to create #{conn.params["endpoint"]}'}")
-    end
-  end
-
-  # This route can be hit when forward slashes are encoded as %2F
-  delete "/site/:endpoint" do
-    case Site.delete_site(endpoint: conn.params["endpoint"]) do
-      {:ok, _} ->
-        send_resp(conn, 201, "{:ok, '#{conn.params["endpoint"]} has been deleted'}")
-
-      _ ->
-        IO.inspect("Pattern matched catch all")
-        send_resp(conn, 400, "{:error, 'Unable to delete #{conn.params["endpoint"]}'}")
-    end
-  end
+  forward("/site", to: SituationRoom.Site.Controller)
 
   match _ do
-    send_resp(conn, 404, "We couldn't find 'er :(")
+    send_resp(conn, 404, "We couldn't the specified route :(")
   end
 end
---
+++ lib/situation_room/site_controller.ex
@@ -0,0 +1,79 @@
+defmodule SituationRoom.Site.Controller do
+  alias SituationRoom.Site
+  use Plug.Router
+
+  plug(:match)
+  # If we are going to include plug.Parsers, they go right here
+  plug(:dispatch)
+
+  # Get a site by name
+  get "/name/:name" do
+    get_site_by(conn, name: conn.params["name"])
+  end
+
+  # Get a site by endpoint/url
+  get "/endpoint/:endpoint" do
+    get_site_by(conn, endpoint: conn.params["endpoint"])
+  end
+
+  # Get a site by id
+  get "/id/:id" do
+    get_site_by(conn, id: conn.params["id"])
+  end
+
+  # This route can be hit when forward slashes are encoded as %2F
+  post "/:name/:endpoint" do
+    case res = Site.create_site(conn.params["name"], conn.params["endpoint"]) do
+      {:ok, _} ->
+        send_resp(conn, 201, "{:ok, 'Successfully created #{conn.params["endpoint"]}'}")
+
+      # We need to pattern match errors for invalid URL
+      {:error,
+       %Ecto.Changeset{
+         action: nil,
+         changes: %{},
+         errors: [endpoint: errors],
+         data: _,
+         valid?: false
+       }} ->
+        send_resp(conn, 400, "{:error, '#{elem(errors, 0)}'}")
+
+      _ ->
+        send_resp(conn, 400, "{:error, 'Unable to create #{conn.params["endpoint"]}'}")
+    end
+  end
+
+  # This route can be hit when forward slashes are encoded as %2F
+  delete "/:endpoint" do
+    case Site.delete_site(endpoint: conn.params["endpoint"]) do
+      {:ok, _} ->
+        send_resp(conn, 201, "{:ok, '#{conn.params["endpoint"]} has been deleted'}")
+
+      _ ->
+        IO.inspect("Pattern matched catch all")
+        send_resp(conn, 400, "{:error, 'Unable to delete #{conn.params["endpoint"]}'}")
+    end
+  end
+
+  match _ do
+    send_resp(conn, 404, "We couldn't find the specified site route.. ")
+  end
+
+  defp get_site_by(conn, field) do
+    case res = Site.get_site(field) do
+      %SituationRoom.Site{} = res ->
+        send_resp(
+          conn,
+          200,
+          "{:ok, {'name': #{Map.get(res, :name)}, 'endpoint': '#{Map.get(res, :endpoint)}', 'id': '#{Map.get(res, :id)}'}}"
+        )
+
+      _ ->
+        send_resp(
+          conn,
+          400,
+          "{:error, '#{elem(List.first(field), 1)} was not be found.. Please make sure everything is entered correctly.'}"
+        )
+    end
+  end
+end
