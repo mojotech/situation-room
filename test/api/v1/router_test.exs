2049157aa4560f50b9b4cab7be1b8f2ad28635ce
Zach Robert <zrobert@mojotech.com>
2022-05-11 14:21:24 -0400

    [test] refac tests for controller & add get tests

--- test/api/v1/router_test.exs
+++ test/api/v1/router_test.exs
@@ -17,17 +17,4 @@ defmodule SituationRoom.RouterTest do
     assert conn.status == 200
     assert conn.resp_body == "{:ok, Mojo Pong}"
   end
-
-  test "post a site" do
-    conn = conn(:post, "/site/testName/https:%2F%2Ftest.com")
-    conn = Router.call(conn, @opts)
-    assert conn.state == :sent
-    assert conn.status == 201
-    assert conn.resp_body == "{:ok, 'Successfully created https://test.com'}"
-    conn = conn(:delete, "/site/https:%2F%2Ftest.com")
-    conn = Router.call(conn, @opts)
-    assert conn.state == :sent
-    assert conn.status == 201
-    assert conn.resp_body == "{:ok, 'https://test.com has been deleted'}"
-  end
 end
