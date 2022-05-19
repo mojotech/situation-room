e91e9c6a8075a7259306c06161081c9f7e9eaba3
Zach Robert <zrobert@mojotech.com>
2022-05-19 11:50:24 -0400

    [feat] dogfood frontend start

---
+++ client/src/common/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const Footer: React.FC = () => {
+    return (
+        <footer>
+            One dogfood footer
+        </footer>
+    )
+}
---
+++ client/src/modules/home/Home.tsx
@@ -0,0 +1,10 @@
+import * as react from "react";
+
+export const Home: React.FC = () => {
+
+    return (
+        <h1>
+            Welcome to a Dogfood version of THE Situation Room!
+        </h1>
+    )
+}
