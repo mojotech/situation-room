92901c442238800b54c9f10e08c0a3a6ece8d398
Craig Blanchette <craig.blanchette@gmail.com>
2022-06-03 15:20:52 -0400

    Fix tests to work with new controller

--- test/situation_room_web/controllers/site_check_controller_test.exs
+++ test/situation_room_web/controllers/site_check_controller_test.exs
@@ -2,27 +2,7 @@ defmodule SituationRoomWeb.ControllerCheckTest do
   use SituationRoomWeb.ConnCase, async: true
   use ExUnit.Case, async: false
 
-  alias SituationRoom.Site
-
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(SituationRoom.Repo)
-
-    %{site: seed()}
-  end
-
-  test "test GET all site pings", %{conn: conn, site: site} do
-    response =
-      conn
-      |> get(Routes.site_check_path(conn, :check, site.id))
-
-    assert response.state == :sent
-    assert response.status == 200
-  end
-
-  defp seed do
-    {:ok, site} =
-      Site.create_site(%{"name" => "testName", "endpoint" => "https://test.com", "interval" => 60})
-
-    site
   end
 end
--- test/situation_room_web/controllers/site_controller_test.exs
+++ test/situation_room_web/controllers/site_controller_test.exs
@@ -2,13 +2,10 @@ defmodule SituationRoom.Site.ControllerTest do
   use SituationRoomWeb.ConnCase, async: true
   use ExUnit.Case, async: false
 
-  alias SituationRoom.Site
-
-  @test_site {"testName", "https://test.com", "https:%2F%2Ftest.com", 60}
+  @test_site {"testName", "https://test.com", "https:%2F%2Ftest.com"}
   @test_name elem(@test_site, 0)
   @test_url elem(@test_site, 1)
-  @test_url_http elem(@test_site, 2)
-  @test_interval elem(@test_site, 3)
+  @test_interval 60
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(SituationRoom.Repo)
@@ -16,35 +13,25 @@ defmodule SituationRoom.Site.ControllerTest do
   end
 
   describe "test CRUD methods" do
-    test "test POST site", %{conn: conn, site: site} do
-      response =
-        conn
-        |> post(
-          Routes.site_path(conn, :create),
-          %{"name" => @test_name, "endpoint" => @test_url, "interval" => @test_interval}
-        )
-        |> json_response(201)
-
-      assert site.id + 1 == response["id"]
+    test "test POST site", %{conn: conn} do
+      conn
+      |> post(
+        Routes.site_path(conn, :create),
+        %{
+          "site" => %{
+            "name" => @test_name,
+            "endpoint" => @test_url,
+            "interval" => @test_interval
+          }
+        }
+      )
+      |> response(302)
     end
 
     test "test DEL site", %{conn: conn, site: site} do
       conn
       |> delete(Routes.site_path(conn, :delete, site.id))
-      |> json_response(204)
-    end
-
-    test "invalid test POST site", %{conn: conn} do
-      conn
-      |> post(
-        Routes.site_path(conn, :create),
-        %{
-          "name" => @test_name,
-          "endpoint" => "#{@test_url_http}badhost",
-          "interval" => @test_interval
-        }
-      )
-      |> json_response(400)
+      |> response(302)
     end
 
     test "invalid test GET site - does not exist", %{conn: conn} do
@@ -54,13 +41,16 @@ defmodule SituationRoom.Site.ControllerTest do
     end
 
     test "invalid test DEL site - nonexistant id", %{conn: conn} do
-      conn
-      |> delete(Routes.site_path(conn, :delete, 9_915_809))
-      |> json_response(404)
+      assert_error_sent(:not_found, fn ->
+        conn
+        |> delete(Routes.site_path(conn, :delete, 9_915_809))
+      end)
     end
   end
 
   defp seed do
+    alias SituationRoom.Site
+
     {:ok, site} =
       Site.create_site(%{"name" => @test_name, "endpoint" => @test_url, "interval" => 60})
 
