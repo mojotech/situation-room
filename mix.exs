d14d5eb4aaa2462522216b9561044ed2076e973d
malssid <mlalssid@gmail.com>
2022-06-06 10:14:24 -0400

    feat: setup petal components

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule SituationRoom.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+      {:petal_components, "~> 0.16"}
     ]
   end
 
