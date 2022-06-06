063e4144adcc60316de0263b34946ef2b0df2073
malssid <mlalssid@gmail.com>
2022-06-06 10:13:53 -0400

    feat: setup tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :situation_room, SituationRoomWeb.Endpoint,
   secret_key_base: "Cq48lmgm/a2n6IxcleWMcxG3cNXcaTmlq70nmj+lBKKbwN/r3UP8KNHjDvNIGspc",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
