fbd8563dcfd89b46b2d017477cc4a4b3653f5c61
Brian Lavall <brian@mojotech.com>
2022-06-07 14:15:04 -0400

    fix: tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,15 +1,24 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     './js/**/*.js',
     '../lib/*_web.ex',
-    '../lib/*_web/**/*.*ex'
+    '../lib/*_web/**/*.*ex',
+    '../deps/petal_components/**/*.*ex'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+        secondary: colors.orange,
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
-  ]
+  ],
+  darkMode: "class"
 }
