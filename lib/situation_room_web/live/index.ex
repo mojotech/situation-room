9b15ff25f9f419098fcf853e0a514b0f53d9cd1d
Brian Lavall <brian@mojotech.com>
2022-06-10 15:54:35 -0400

    feat: convert homepage to liveview template

--- lib/situation_room_web/live/index.ex
+++ lib/situation_room_web/live/index.ex
@@ -1,14 +1,13 @@
-defmodule SituationRoomWeb.Live.Sites.Index do
+defmodule SituationRoomWeb.Live.Index do
   @moduledoc false
 
   use SituationRoomWeb, :live_view
 
   alias SituationRoom.Sites
 
-  @impl true
   def mount(_params, %{}, socket) do
     sites = Sites.get_all_sites()
 
-    {:ok, assign(socket, %{sites: sites})}
+    {:ok, assign(socket, :sites, sites)}
   end
 end
