676d89b7482aea6bb600382465032c41438a7f49
Brian Lavall <brian@mojotech.com>
2022-06-10 16:16:36 -0400

    feat: impl liveview on sites index

--- lib/situation_room/sites.ex
+++ lib/situation_room/sites.ex
@@ -6,6 +6,8 @@ defmodule SituationRoom.Sites do
   alias SituationRoom.Repo
   alias SituationRoom.Site
 
+  @topic "sites"
+
   # Get a site from the database by one specific field
   # param ex: (name: "mojotech") or (endpoint: "http://mojo.com")
   def get_site(param) do
@@ -25,17 +27,31 @@ defmodule SituationRoom.Sites do
     %Site{}
     |> Site.changeset(params)
     |> Repo.insert()
+    |> broadcast(:site_created)
   end
 
   # Delete a site from the database by specifying specific field
   # param ex: (name: "mojotech") or (endpoint: "http://mojo.com")
   def delete_site(site) do
     Repo.delete(site)
+    |> broadcast(:site_deleted)
   end
 
   def update_site(%Site{} = site, attrs) do
     site
     |> Site.changeset(attrs)
     |> Repo.update()
+    |> broadcast(:site_updated)
+  end
+
+  def subscribe do
+    Phoenix.PubSub.subscribe(SituationRoom.PubSub, @topic)
+  end
+
+  defp broadcast({:error, _reason} = error, _event), do: error
+
+  defp broadcast({:ok, site}, event) do
+    Phoenix.PubSub.broadcast(SituationRoom.PubSub, @topic, {event, site})
+    {:ok, site}
   end
 end
