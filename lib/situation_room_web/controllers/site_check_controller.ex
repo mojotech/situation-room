0dd80ed90fa85e5e16a39e81b343be0ded8c9977
Zach Robert <zrobert@mojotech.com>
2022-06-14 10:34:44 -0400

    [feat] add button to run site checks

--- lib/situation_room_web/controllers/site_check_controller.ex
+++ lib/situation_room_web/controllers/site_check_controller.ex
@@ -1,9 +1,29 @@
 defmodule SituationRoomWeb.SiteCheckController do
   use SituationRoomWeb, :controller
-  alias SituationRoom.Checks
+  alias SituationRoom.{Checks, Outages, Sites}
+  alias SituationRoom.Check.Manager
 
   def check(conn, _params) do
     conn
     |> json(Checks.get_all_checks())
+  end
+
+  def edit(conn, %{"id" => id}) do
+    site = Sites.get_site!(id)
+    Manager.head_check(site)
+
+    case Outages.get_last(id) do
+      %SituationRoom.Site.Outage{resolved: result} ->
+        case result do
+          true ->
+            conn |> render("edit.html", site: site, check: "resolved")
+
+          false ->
+            conn |> render("edit.html", site: site, check: "down")
+        end
+
+      nil ->
+        conn |> render("edit.html", site: site, check: "okay")
+    end
   end
 end
